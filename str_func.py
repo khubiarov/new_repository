70a373892b48f97c540848b544b598a978ea04ca
khubiarov <72666523+khubiarov@users.noreply.github.com>
2023-03-22 05:09:03 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_func(source_string):
     return source_string.upper()
+
+
+def first_letter():
+    '''возвращает принятую строку с первой буквой в верхнем регистре'''
+    numb = len(source_string)
+    return f"{source_string[0].upper()}{source_string[1:numb]}"
