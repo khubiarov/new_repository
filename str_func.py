95f21ed0514ec8bdb1f52d00f91e73cbb5907d77
khubiarov <72666523+khubiarov@users.noreply.github.com>
2023-03-22 05:04:29 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,11 +1,7 @@
 def str_func(source_string):
-
     return source_string.upper()
 
 
-def first_letter(source_string):
-    numb = len(source_string)
-    return f"{source_string[0].upper()}{source_string[1:numb]}"
 
 
 
